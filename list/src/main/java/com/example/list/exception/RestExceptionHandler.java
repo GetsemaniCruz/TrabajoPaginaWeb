69d3243f79e5eb3bca5d832473fa1937b20a2644
Getsemaní Cruz Millán <20243ds149@utez.edu.mx>
2025-12-01 13:49:56 -0600

    list pt2

---
+++ list/src/main/java/com/example/list/exception/RestExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.example.list.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+    //captura errores de validacion del @Valic en @RequestBody
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidation(MethodArgumentNotValidException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("message", "Error de validación");
+        Map<String, String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .collect(Collectors.toMap(
+                        fe -> fe.getField(),
+                        fe -> fe.getDefaultMessage(),
+                        (existing, replacement) -> existing  // si hay duplicados, usa el primero
+                ));
+        body.put("errors", errors);
+        body.put("status", HttpStatus.BAD_REQUEST.value());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
+    }
+}
